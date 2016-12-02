c1aca02cd064f6bfc656d0ddff476751dd24b8a5
jesus0084 <jesus0084@naver.com>
2016-12-02 21:36:18 +0900

    first commit

---
+++ MyCalendarInput/app/src/main/java/com/example/user/mycalendarinput/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.user.mycalendarinput;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Date;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,  AdapterView.OnItemClickListener {
+    ArrayList<String> mItems;
+    ArrayAdapter<String> adapter;
+    TextView textYear;
+    TextView textMon;
+    int year;
+    int mon;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textYear = (TextView) this.findViewById(R.id.yearedit);
+        textMon = (TextView) this.findViewById(R.id.monthedit);
+
+
+
+        mItems = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mItems);
+
+        GridView gird = (GridView) this.findViewById(R.id.grid1);
+        gird.setAdapter(adapter);
+        gird.setOnItemClickListener(this);
+
+        Date date = new Date();// 오늘에 날짜를 세팅 해준다.
+        int year = date.getYear() + 1900;
+        int mon = date.getMonth() + 1;
+        textYear.setText(year + "");
+        textMon.setText(mon + "");
+        fillDate(year, mon);
+        Button btnmove = (Button) this.findViewById(R.id.gotodate);
+        Button next = (Button) this.findViewById(R.id.next);
+        Button priv = (Button) this.findViewById(R.id.priv);
+
+        this.year = Integer.parseInt(textYear.getText().toString());
+        this.mon = Integer.parseInt(textMon.getText().toString());
+
+        btnmove.setOnClickListener(this);
+        next.setOnClickListener(this);
+        priv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View arg0) {
+// TODO Auto-generated method stub
+        if (arg0.getId() == R.id.gotodate) {
+            year = Integer.parseInt(textYear.getText().toString());
+            mon = Integer.parseInt(textMon.getText().toString());
+            fillDate(year, mon);
+        }
+        else if(arg0.getId() == R.id.priv){
+            mon -= 1;
+            if(mon<=0){
+                year -= 1;
+                mon=12;
+            }
+            fillDate(year, mon);
+            textYear.setText(Integer.toString(year));
+            textMon.setText(Integer.toString(mon));
+        }
+        else if(arg0.getId() == R.id.next){
+            mon += 1;
+            if(mon>=13){
+                year += 1;
+                mon=1;
+            }
+            fillDate(year, mon);
+            textYear.setText(Integer.toString(year));
+            textMon.setText(Integer.toString(mon));
+        }
+
+    }
+
+    private void fillDate(int year, int mon) {
+        mItems.clear();
+        mItems.add("일");
+        mItems.add("월");
+        mItems.add("화");
+        mItems.add("수");
+        mItems.add("목");
+        mItems.add("금");
+        mItems.add("토");
+        Date current = new Date(year - 1900, mon - 1, 1);
+        int day = current.getDay(); // 요일도 int로 저장.
+        for (int i = 0; i < day; i++) {
+            mItems.add("");
+        }
+        current.setDate(32);// 32일까지 입력하면 1일로 바꿔준다.
+        int last = 32 - current.getDate();
+        for (int i = 1; i <= last; i++) {
+            mItems.add(i + "");
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+// TODO Auto-generated method stub
+        if (mItems.get(arg2).equals("")) {
+            ;
+        } else {
+            Intent intent = new Intent(this, ExToday.class);//해당 일을 눌렸을때
+            intent.putExtra("Param1", textYear.getText().toString() + "/"
+                    + textMon.getText().toString() + "/" + mItems.get(arg2));
+            startActivity(intent);
+        }
+    }
+}
